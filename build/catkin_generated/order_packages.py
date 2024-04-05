433d3c6039c9678fab202da864f27c85ba3059ac
Qubi <kuba1102@gmail.com>
2024-04-05 11:35:47 +0200

    Remove unused files and directories

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/qubi/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/qubi/catkin_ws/devel;/home/qubi/projects/avoidance_node/devel;/opt/ros/noetic'.split(';') if '/home/qubi/catkin_ws/devel;/home/qubi/projects/avoidance_node/devel;/opt/ros/noetic' != '' else []

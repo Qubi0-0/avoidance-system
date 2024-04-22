93230c9c4afed313ff4b4ea136f351054c53c329
Qubi <kuba1102@gmail.com>
2024-04-22 19:42:25 +0200

    Adjust FLIGHT_ALT value in point_cloud_node.py for drone flight

--- src/avoidance/src/point_cloud_node.py
+++ src/avoidance/src/point_cloud_node.py
@@ -58,28 +58,11 @@ class Avoidance:
         self.pose = rospy.Subscriber(
             "/mavros/local_position/pose", PoseStamped, self.positionCallback)
 
-        self.pub_pose = rospy.Publisher(
-            "/mavros/setpoint_position/local", PoseStamped, queue_size=10)
-
         self.pub_vel = rospy.Publisher(
             "/mavros/setpoint_velocity/cmd_vel_unstamped", Twist, queue_size=1)
-
-        self.max_hor_vel = rospy.ServiceProxy(
-            '/mavros/param/set', mavros_msgs.srv.ParamSet)
-        
-        self.pub_accel = rospy.Publisher(
-            "/mavros/setpoint_accel/accel", Vector3Stamped, queue_size=1)
-
-        rospy.wait_for_service("/mavros/cmd/arming")
-        self.arming_client = rospy.ServiceProxy("mavros/cmd/arming", CommandBool)    
-
-        rospy.wait_for_service("/mavros/set_mode")
-        self.set_mode_client = rospy.ServiceProxy("mavros/set_mode", SetMode)
         
         self.sub_cloud = rospy.Subscriber(
             '/iris/camera/depth/points', pc2.PointCloud2, self.cloud_callback, queue_size=1)
-
-        self.state_sub = rospy.Subscriber("mavros/state", State, callback=self.state_callback)
         
         self.marker_pub = rospy.Publisher('visualization_marker_array', MarkerArray, queue_size=10)
 
@@ -102,17 +85,8 @@ class Avoidance:
             PosePointRPY(0, 180, FLIGHT_ALT, 0, 0, fixed_yaw)
         ]
         self.vel = Twist()
-        self.odom = Odometry()
-        self.state = 0
-        self.tfBuffer = tf2_ros.Buffer()
-        self.tfListener = tf2_ros.TransformListener(self.tfBuffer)
         self.local_pose = PoseStamped()
         self.rate = rospy.Rate(20)
-        self.current_state = State()
-        self.offb_set_mode = SetModeRequest()
-        self.arm_cmd = CommandBoolRequest()
-        self.offb_set_mode.custom_mode = 'OFFBOARD'
-        self.arm_cmd.value = True
         self.last_req = rospy.Time.now()
         self.bridge = CvBridge()
         self.mean_mat = None
@@ -132,14 +106,7 @@ class Avoidance:
                     rpy[0], rpy[1], rpy[2])        
 
         return result
-
-    def set_horizontal_velocity(self, max_velocity):
-        rospy.wait_for_service('/mavros/param/set')
-        try:
-            self.max_hor_vel(param_id="MPC_XY_VEL_ALL", value=mavros_msgs.msg.ParamValue(real=max_velocity)) # type: ignore
-        except rospy.ServiceException as exception:
-            print(f"Service max_horizontal_velocity (MPC_XY_VEL_MAX) call failed: {exception}")
-
+    
     def positionCallback(self, msg: PoseStamped):
         self.local_pose.pose.position.x = msg.pose.position.x
         self.local_pose.pose.position.y = msg.pose.position.y
@@ -159,76 +126,7 @@ class Avoidance:
         # Group the points
         if cloud_points:
             self.clusters = self.group_points(self.obstacles)
-
-    def switch_to_offboard(self, type):
-        if(self.current_state.mode != type and (rospy.Time.now() - self.last_req) > rospy.Duration(5)):
-            if(self.set_mode_client.call(self.offb_set_mode).mode_sent == True):
-                rospy.loginfo(f"{type} enabled")
             
-            self.last_req = rospy.Time.now()
-        else:
-            if(not self.current_state.armed and (rospy.Time.now() - self.last_req) > rospy.Duration(5)):
-                if(self.arming_client.call(self.arm_cmd).success == True):
-                    rospy.loginfo("Vehicle armed")
-            
-                self.last_req = rospy.Time.now()
-
-    def publish_pose(self, goal: PosePointRPY):
-        pose = PoseStamped()
-        pose.header.stamp = rospy.Time.now()
-        pose.pose.position.x = goal.pos.pose.position.x
-        pose.pose.position.y = goal.pos.pose.position.y
-        pose.pose.position.z = goal.pos.pose.position.z
-        pose.pose.orientation.x = goal.pos.pose.orientation.x
-        pose.pose.orientation.y = goal.pos.pose.orientation.y
-        pose.pose.orientation.z = goal.pos.pose.orientation.z
-        pose.pose.orientation.w = goal.pos.pose.orientation.w
-        self.pub_pose.publish(pose)
-
-    def publish_takeoff_point(self):
-        # self.set_horizontal_velocity(2)
-        self.flight_status = Status.Takeoff
-        start_pose =  PoseStamped()
-        start_pose.pose.position.x = self.local_pose.pose.position.x
-        start_pose.pose.position.y = self.local_pose.pose.position.x
-        start_pose.pose.position.z = FLIGHT_ALT
-        
-        quaternion = transformations.quaternion_from_euler(0, 0, self.yaw_angle)
-        start_pose.pose.orientation.x = quaternion[0]
-        start_pose.pose.orientation.y = quaternion[1]
-        start_pose.pose.orientation.z = quaternion[2]
-        start_pose.pose.orientation.w = quaternion[3]
-        self.pub_pose.publish(start_pose)
-
-    def position_check(self, goal: PosePointRPY):
-        dx = self.local_pose.pose.position.x - goal.pos.pose.position.x
-        dy = self.local_pose.pose.position.y - goal.pos.pose.position.y
-        dz = self.local_pose.pose.position.z - goal.pos.pose.position.z
-        dist2 = dx**2 + dy**2 + dz**2
-        if dist2 > POS_TRESHOLD**2:
-            return False
-
-        curr_orient = self.local_pose.pose.orientation
-        curr_euler = transformations.euler_from_quaternion(
-            [curr_orient.x, curr_orient.y, curr_orient.z, curr_orient.w])
-        cr = abs(curr_euler[0] - math.radians(goal.roll)) < TARGET_ANGL
-        cp = abs(curr_euler[1] - math.radians(goal.pitch)) < TARGET_ANGL
-        cy = abs(curr_euler[2] - math.radians(goal.yaw)) < TARGET_ANGL
-        
-        in_range = cr and cp and cy
-        if in_range:
-            rospy.loginfo(f"Desired Position Reached X:{self.fixed_positions[self.state].pos.pose.position.x} Y:{self.fixed_positions[self.state].pos.pose.position.y}")
-        return in_range 
-
-    def updateState(self):
-        if self.state < len(self.fixed_positions):
-            self.state += 1
-            self.fixed_positions[self.state].pos.pose.position.y = self.local_pose.pose.position.y
-            if self.state >= len(self.fixed_positions):
-                self.state = len(self.fixed_positions) -1
-        else:
-            self.state = 1
-
     def group_points(self, cloud_points, eps=0.5, min_samples=10, max_distance=30):
         # distances = np.linalg.norm(cloud_points, axis=1)
 
@@ -316,7 +214,7 @@ class Avoidance:
 
         # rospy.loginfo(f"\n Atractive: \n x: {attractive_force[0]}, y: {attractive_force[1]}, z: {attractive_force[2]} \n Repulsive:\n x: {repulsive_force[0]}, y: {repulsive_force[1]}, z: {repulsive_force[2]} \n Value of Total Force: {total_force} \n")
         if rospy.Time.now() - self.last_published > rospy.Duration(0, 200000000):
-            self.pub_vel.publish(twist_msg)
+            # self.pub_vel.publish(twist_msg)
             self.last_published = rospy.Time.now()
 
 
@@ -337,9 +235,9 @@ class Avoidance:
             centroid += self.drone_position
 
             # Set the marker's position
-            marker.pose.position.x = centroid[0]
-            marker.pose.position.y = centroid[1]
-            marker.pose.position.z = centroid[2]
+            marker.pose.position.x = 0
+            marker.pose.position.y = 0
+            marker.pose.position.z = 0
             factor = 0.1
             # Set the marker's size proportional to the standard deviation
             marker.scale.x = std_dev * factor
@@ -360,33 +258,9 @@ class Avoidance:
 if __name__ == '__main__':
     rospy.init_node('avoidance_node') 
     avoider = Avoidance()
-    rospy.loginfo("Avoidance node initiated")
+    rospy.loginfo("Marker view node initiated")
 
-    
-    while(not rospy.is_shutdown() and not avoider.current_state.connected):
-        avoider.rate.sleep()
-
-    for i in range(10):   
-        if(rospy.is_shutdown()):
-            break
-        avoider.publish_takeoff_point()
-        avoider.rate.sleep()
-
-    avoider.offb_set_mode.custom_mode = 'OFFBOARD'
-    # avoider.offb_set_mode.custom_mode = 'POSCTL'
-    avoider.arm_cmd.value = True
-
-    avoider.last_req = rospy.Time.now()  
-    rospy.loginfo("Preparing to takeoff")
     while(not rospy.is_shutdown()):
-        avoider.switch_to_offboard(avoider.offb_set_mode.custom_mode)
-
-
-        if avoider.flight_status == Status.FlyMode:
-            avoider.spin()
-        elif (avoider.local_pose.pose.position.z > FLIGHT_ALT - HEIGHT_TRESHOLD) and avoider.flight_status == Status.Takeoff:
-            avoider.flight_status = Status.FlyMode          
-            rospy.loginfo("STATUS Change to FlyMode")
-            
-        else:
-            avoider.publish_takeoff_point()
+ 
+        avoider.spin()
+ 

e93368587243b1367e5717111362a2719010e927
Qubi <kuba1102@gmail.com>
2024-05-10 14:34:33 +0200

    Refactor octomap.cpp to remove unused code and comments - still dont know how to read octomap

--- src/cpp_avoidance/src/octomap.cpp
+++ src/cpp_avoidance/src/octomap.cpp
@@ -13,6 +13,7 @@ void AvoidanceOctomap::cloudCallback(const sensor_msgs::PointCloud2::ConstPtr& c
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromPCLPointCloud2(pcl_pc2, *cloud);
 
+
     pcl::PassThrough<pcl::PointXYZ> pass;
     pass.setInputCloud(cloud);
     pass.setFilterFieldName("z");
@@ -21,18 +22,18 @@ void AvoidanceOctomap::cloudCallback(const sensor_msgs::PointCloud2::ConstPtr& c
     PointCloudPtr cloud_downranged(new pcl::PointCloud<pcl::PointXYZ>);
     cloud_downranged = cloud;
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_transformed(new pcl::PointCloud<pcl::PointXYZ>());
-    geometry_msgs::TransformStamped transform_stamped;
-    try {
-        transform_stamped = tf_buffer_.lookupTransform("odom", "camera_link", ros::Time::now(), ros::Duration(1));
-        pcl_ros::transformPointCloud(*cloud_downranged, *cloud_transformed, transform_stamped.transform);
-    } catch (tf2::TransformException& ex) {
-        ROS_WARN("%s", ex.what());
-        return;
-    }
+    // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_transformed(new pcl::PointCloud<pcl::PointXYZ>());
+    // geometry_msgs::TransformStamped transform_stamped;
+    // try {
+    //     transform_stamped = tf_buffer_.lookupTransform("odom", "camera_link", ros::Time::now(), ros::Duration(1));
+    //     pcl_ros::transformPointCloud(*cloud_downranged, *cloud_transformed, transform_stamped.transform);
+    // } catch (tf2::TransformException& ex) {
+    //     ROS_WARN("%s", ex.what());
+    //     return;
+    // }
 
     octomap::Pointcloud octomap_cloud;
-    for (const auto& point : cloud_transformed->points) {
+    for (const auto& point : cloud_downranged->points) {
         octomap_cloud.push_back(point.x, point.y, point.z);
     }
 

52fecffc5b20a82f5844006301897413af8f2189
Qubi <kuba1102@gmail.com>
2024-05-15 12:26:49 +0200

    reafactor: TF2 tranform updated to work properly with pointcloud2

--- src/cpp_avoidance/include/avoidance/octomap.h
+++ src/cpp_avoidance/include/avoidance/octomap.h
@@ -3,7 +3,6 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/Twist.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <octomap/octomap.h>
 #include <octomap_msgs/Octomap.h>
@@ -11,26 +10,24 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl/segmentation/extract_clusters.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_sensor_msgs/tf2_sensor_msgs.h>
-
-using PointCloudPtr = pcl::PointCloud<pcl::PointXYZ>::Ptr;
+#include <visualization_msgs/MarkerArray.h>
 
 class AvoidanceOctomap {
 public:
     AvoidanceOctomap(const ros::NodeHandle& nh);
 
     void cloudCallback(const sensor_msgs::PointCloud2::ConstPtr& cloud_msg);
-    void generateOctomap();
 
 private:
     ros::NodeHandle nh_;
     ros::Subscriber cloud_sub_;
     ros::Publisher octomap_pub_;
-    ros::Publisher grid_pub_;
+    ros::Publisher marker_array_pub_;
     octomap::OcTree octree_;
     tf2_ros::Buffer tf_buffer_;
+    tf2_ros::TransformListener tf_listener_;
 };
 
 #endif // AVOIDANCE_OCTOMAP_H
--- src/cpp_avoidance/src/octomap.cpp
+++ src/cpp_avoidance/src/octomap.cpp
@@ -1,13 +1,13 @@
 #include "octomap.h"
 #include "pcl_ros/transforms.h"
 #include <pcl/filters/passthrough.h>
-#include <nav_msgs/OccupancyGrid.h>
-#include <octomap_msgs/Octomap.h>
 
-AvoidanceOctomap::AvoidanceOctomap(const ros::NodeHandle& nh) : nh_(nh), octree_(0.1) {
+AvoidanceOctomap::AvoidanceOctomap(const ros::NodeHandle& nh)
+    : nh_(nh), octree_(0.1), tf_listener_(tf_buffer_) {
+    tf_buffer_.setUsingDedicatedThread(true);
     cloud_sub_ = nh_.subscribe<sensor_msgs::PointCloud2>("/iris/camera/depth/points", 1, &AvoidanceOctomap::cloudCallback, this);
     octomap_pub_ = nh_.advertise<octomap_msgs::Octomap>("/output/octomap_topic", 1);
-    grid_pub_ = nh_.advertise<nav_msgs::OccupancyGrid>("/output/occupancy_grid",5);
+    marker_array_pub_ = nh_.advertise<visualization_msgs::MarkerArray>("marker_array", 1);
 }
 
 void AvoidanceOctomap::cloudCallback(const sensor_msgs::PointCloud2::ConstPtr& cloud_msg) {
@@ -16,82 +16,88 @@ void AvoidanceOctomap::cloudCallback(const sensor_msgs::PointCloud2::ConstPtr& c
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromPCLPointCloud2(pcl_pc2, *cloud);
 
+    // Downsample the cloud by selecting every Nth point
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_downsampled(new pcl::PointCloud<pcl::PointXYZ>());
+    for (int i = 0; i < cloud->points.size(); i += 1000) {
+        cloud_downsampled->points.push_back(cloud->points[i]);
+    }
 
     pcl::PassThrough<pcl::PointXYZ> pass;
-    pass.setInputCloud(cloud);
+    pass.setInputCloud(cloud_downsampled);
     pass.setFilterFieldName("z");
-    pass.setFilterLimits(0.0, 29); // Keep points at a distance of 0 to "range" meters
-    pass.filter(*cloud);
-    PointCloudPtr cloud_downranged(new pcl::PointCloud<pcl::PointXYZ>);
-    cloud_downranged = cloud;
+    pass.setFilterLimits(0.0, 29); 
+    pass.filter(*cloud_downsampled);
 
-    // pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_transformed(new pcl::PointCloud<pcl::PointXYZ>());
-    // geometry_msgs::TransformStamped transform_stamped;
-    // try {
-    //     transform_stamped = tf_buffer_.lookupTransform("odom", "camera_link", ros::Time::now(), ros::Duration(1));
-    //     pcl_ros::transformPointCloud(*cloud_downranged, *cloud_transformed, transform_stamped.transform);
-    // } catch (tf2::TransformException& ex) {
-    //     ROS_WARN("%s", ex.what());
-    //     return;
-    // }
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_downranged(new pcl::PointCloud<pcl::PointXYZ>);
+    cloud_downranged = cloud_downsampled;
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_transformed(new pcl::PointCloud<pcl::PointXYZ>());
+    geometry_msgs::TransformStamped transform_stamped;
+    try {
+        if (tf_buffer_.canTransform("odom", "camera_link", ros::Time(0), ros::Duration(1.0))) {
+            transform_stamped = tf_buffer_.lookupTransform("odom", "camera_link", ros::Time(0));
+            pcl_ros::transformPointCloud(*cloud_downranged, *cloud_transformed, transform_stamped.transform);
+        } else {
+            ROS_WARN("Transform from 'odom' to 'camera_link' not available");
+            return;
+        }
+    } catch (tf2::TransformException& ex) {
+        ROS_WARN("%s", ex.what());
+        return;
+    }
 
     octomap::Pointcloud octomap_cloud;
-    for (const auto& point : cloud_downranged->points) {
+    for (const auto& point : cloud_transformed->points) {
         octomap_cloud.push_back(point.x, point.y, point.z);
     }
 
-        octree_.insertPointCloud(octomap_cloud, octomap::point3d(0, 0, 0));
-
-        octree_.updateInnerOccupancy();
-        octree_.toMaxLikelihood();
-        
+    octree_.insertPointCloud(octomap_cloud, octomap::point3d(0, 0, 0));
+    octree_.updateInnerOccupancy();
+    octree_.toMaxLikelihood();
 
     octomap_msgs::Octomap octomap_msg;
     octomap_msg.header.stamp = ros::Time::now();
     octomap_msg.header.frame_id = "camera_link";  
     octomap_msgs::binaryMapToMsg(octree_, octomap_msg);
 
-
     octomap_pub_.publish(octomap_msg);
 
-        
-        nav_msgs::OccupancyGrid grid_msg;
-        double min_x = std::numeric_limits<double>::max();
-        double min_y = std::numeric_limits<double>::max();
+    // Create a MarkerArray
+    visualization_msgs::MarkerArray marker_array;
 
-        for (octomap::OcTree::leaf_iterator it = octree_.begin_leafs(), end = octree_.end_leafs(); it != end; ++it) {
-            min_x = std::min(min_x, it.getX());
-            min_y = std::min(min_y, it.getY());
+    // Iterate over the leaf nodes of the Octomap
+    for (octomap::OcTree::leaf_iterator it = octree_.begin_leafs(), end = octree_.end_leafs(); it != end; ++it) {
+        if (octree_.isNodeOccupied(*it)) {
+            // Create a marker for each occupied node
+            visualization_msgs::Marker marker;
+            marker.header.frame_id = "camera_link";
+            marker.header.stamp = ros::Time::now();
+            marker.ns = "octomap";
+            marker.id = it.getDepth();
+            marker.type = visualization_msgs::Marker::CUBE;
+            marker.action = visualization_msgs::Marker::ADD;
+            marker.pose.position.x = it.getX();
+            marker.pose.position.y = it.getY();
+            marker.pose.position.z = it.getZ();
+            marker.pose.orientation.x = 0.0;
+            marker.pose.orientation.y = 0.0;
+            marker.pose.orientation.z = 0.0;
+            marker.pose.orientation.w = 1.0;
+            marker.scale.x = it.getSize();
+            marker.scale.y = it.getSize();
+            marker.scale.z = it.getSize();
+            marker.color.a = 1.0;
+            marker.color.r = 0.0;
+            marker.color.g = 1.0;
+            marker.color.b = 0.0;
+
+            // Add the marker to the MarkerArray
+            marker_array.markers.push_back(marker);
         }
+    }
 
-        grid_msg.info.origin.position.x = min_x;
-        grid_msg.info.origin.position.y = min_y;
-        // Set the size of the grid
-        grid_msg.info.width = 100;  // Change this to your desired width
-        grid_msg.info.height = 100;  // Change this to your desired height
-        grid_msg.data = std::vector<int8_t>(grid_msg.info.width * grid_msg.info.height, -1);
-        grid_msg.header.stamp = ros::Time::now();
-        grid_msg.header.frame_id = "camera_link";  
-        grid_msg.info.resolution = 1.1; 
-
-      
-
-        for (octomap::OcTree::leaf_iterator it = octree_.begin_leafs(), end = octree_.end_leafs(); it != end; ++it) {
-            if (octree_.isNodeOccupied(*it)) {
-                double x = it.getX();
-                double y = it.getY();
-                int i = (x - grid_msg.info.origin.position.x) / grid_msg.info.resolution;
-                int j = (y - grid_msg.info.origin.position.y) / grid_msg.info.resolution;
-                int index = i + j * grid_msg.info.width;
-                if (index >= 0 && index < grid_msg.data.size()) {
-                    grid_msg.data[index] = 100;
-                } else {
-                    ROS_WARN("Invalid index: %d (grid size: %zu)", index, grid_msg.data.size());
-                }
-            }
-        }
-
-        grid_pub_.publish(grid_msg);
+    // Publish the MarkerArray
+    marker_array_pub_.publish(marker_array);
 }
 
 int main(int argc, char** argv) {

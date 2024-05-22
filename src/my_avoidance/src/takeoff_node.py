e52af1375aff563d030b35d0555798e3b5fe1df7
Qubi <kuba1102@gmail.com>
2024-05-22 16:11:22 +0200

    refactor: delete unused code

--- src/my_avoidance/src/takeoff_node.py
+++ src/my_avoidance/src/takeoff_node.py
@@ -4,38 +4,13 @@ import rospy
 from geometry_msgs.msg import PoseStamped, Twist
 from mavros_msgs.msg import State
 from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest
-import mavros_msgs.srv
-from nav_msgs.msg import Odometry
-import math
 from tf import transformations
 
 
-START_ALT = 5 # alt for drone flight
+START_ALT = 10 # alt for drone flight
 M_PI = 3.14159265359
 TARGET_ANGL = 1 * (M_PI / 180.0)
 POS_TRESHOLD = 0.3
-
-
-class PosePointRPY:
-    def __init__(self, x, y, z, roll, pitch, yaw):
-        
-        self.roll = roll
-        self.pitch = pitch
-        self.yaw = yaw
-
-        self.pos = PoseStamped()
-        self.pos.pose.position.x = x
-        self.pos.pose.position.y = y
-        self.pos.pose.position.z = z
-        rollRad = math.radians(roll)
-        pitchRad = math.radians(pitch)
-        yawRad = math.radians(yaw)
-        quaternion = transformations.quaternion_from_euler(rollRad, pitchRad, yawRad)
-        self.pos.pose.orientation.x = quaternion[0]
-        self.pos.pose.orientation.y = quaternion[1]
-        self.pos.pose.orientation.z = quaternion[2]
-        self.pos.pose.orientation.w = quaternion[3]
-
 
 class TakeOff:
     def __init__(self):
@@ -44,13 +19,7 @@ class TakeOff:
 
         self.pub_pose = rospy.Publisher(
             "/mavros/setpoint_position/local", PoseStamped, queue_size=10)
-
-        self.pub_vel = rospy.Publisher(
-            "/mavros/setpoint_velocity/cmd_vel_unstamped", Twist, queue_size=1)
         
-        self.max_hor_vel = rospy.ServiceProxy(
-            '/mavros/param/set', mavros_msgs.srv.ParamSet)
-
         rospy.wait_for_service("/mavros/cmd/arming")
         self.arming_client = rospy.ServiceProxy("mavros/cmd/arming", CommandBool)    
 
@@ -59,10 +28,6 @@ class TakeOff:
 
         self.state_sub = rospy.Subscriber("mavros/state", State, callback=self.state_callback)
 
-        fixed_yaw = 180
-        self.avoid_position = PosePointRPY(1, 0, START_ALT,  0, 0, fixed_yaw)
-        self.vel = Twist()
-        self.odom = Odometry()
         self.local_pose = PoseStamped()
         self.rate = rospy.Rate(20)
         self.current_state = State()
@@ -71,22 +36,6 @@ class TakeOff:
         self.offb_set_mode.custom_mode = 'OFFBOARD'
         self.arm_cmd.value = True
         self.last_req = rospy.Time.now()
-
-    def poseStamped_to_rpy(self, source: PoseStamped):
-        rpy = transformations.euler_from_quaternion([source.pose.orientation.x, source.pose.orientation.y,
-                    source.pose.orientation.z, source.pose.orientation.w])
-
-        result = PosePointRPY(source.pose.position.x, source.pose.position.y, source.pose.position.z,
-                    rpy[0], rpy[1], rpy[2])        
-
-        return result
-
-    def set_horizontal_velocity(self, max_velocity):
-        rospy.wait_for_service('/mavros/param/set')
-        try:
-            self.max_hor_vel(param_id="MPC_XY_VEL_ALL", value=mavros_msgs.msg.ParamValue(real=max_velocity)) # type: ignore
-        except rospy.ServiceException as e:
-            print("Service max_horizontal_velocity (MPC_XY_VEL_MAX) call failed: %s" % e)
 
     def positionCallback(self, msg: PoseStamped):
         self.local_pose.pose.position.x = msg.pose.position.x
@@ -113,54 +62,13 @@ class TakeOff:
             
                 self.last_req = rospy.Time.now()
 
-    def publish_pose(self, goal: PosePointRPY):
-        pose = PoseStamped()
-        pose.header.stamp = rospy.Time.now()
-        pose.pose.position.x = goal.pos.pose.position.x
-        pose.pose.position.y = goal.pos.pose.position.y
-        pose.pose.position.z = goal.pos.pose.position.z
-        pose.pose.orientation.x = goal.pos.pose.orientation.x
-        pose.pose.orientation.y = goal.pos.pose.orientation.y
-        pose.pose.orientation.z = goal.pos.pose.orientation.z
-        pose.pose.orientation.w = goal.pos.pose.orientation.w
-        self.pub_pose.publish(pose)
-
     def publish_takeoff_point(self):
-        self.set_horizontal_velocity(2)
-
         start_pose =  PoseStamped()
         start_pose.pose.position.x = self.local_pose.pose.position.x
         start_pose.pose.position.y = self.local_pose.pose.position.x
         start_pose.pose.position.z = START_ALT
-        rollRad = 0 
-        pitchRad = 0 
-        yawRad = math.radians(240)
-        quaternion = transformations.quaternion_from_euler(rollRad, pitchRad, yawRad)
-        start_pose.pose.orientation.x = quaternion[0]
-        start_pose.pose.orientation.y = quaternion[1]
-        start_pose.pose.orientation.z = quaternion[2]
-        start_pose.pose.orientation.w = quaternion[3]
+        start_pose.pose.orientation = self.local_pose.pose.orientation
         self.pub_pose.publish(start_pose)
-
-    def position_check(self, goal: PosePointRPY):
-        dx = self.local_pose.pose.position.x - goal.pos.pose.position.x
-        dy = self.local_pose.pose.position.y - goal.pos.pose.position.y
-        dz = self.local_pose.pose.position.z - goal.pos.pose.position.z
-        dist2 = dx**2 + dy**2 + dz**2
-        if dist2 > POS_TRESHOLD**2:
-            return False
-
-        curr_orient = self.local_pose.pose.orientation
-        curr_euler = transformations.euler_from_quaternion(
-            [curr_orient.x, curr_orient.y, curr_orient.z, curr_orient.w])
-        cr = abs(curr_euler[0] - math.radians(goal.roll)) < TARGET_ANGL
-        cp = abs(curr_euler[1] - math.radians(goal.pitch)) < TARGET_ANGL
-        cy = abs(curr_euler[2] - math.radians(goal.yaw)) < TARGET_ANGL
-        
-        in_range = cr and cp and cy
-        # if in_range:
-            # rospy.loginfo(f"Desired Position Reached X:{self.fixed_positions[self.state].pos.pose.position.x} Y:{self.fixed_positions[self.state].pos.pose.position.y}")
-        return in_range 
 
 
 if __name__ == '__main__':
